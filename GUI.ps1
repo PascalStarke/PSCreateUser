5fcc985f5aef3c54a66243f3ab8b57287d4d726b
Clijsters <dominique.clijsters@gmail.com>
2014-12-24 13:35:10 +0100

    Work on Ask()
    uncomplete

--- GUI.ps1
+++ GUI.ps1
@@ -5,53 +5,78 @@ Add-Type -AssemblyName System.Windows.Forms
 
 $QuestionType = New-Object psobject -property @{
     YesNo = 0
-    OKCancel = 1
+    OkCancel = 1
     YesNoCancel = 2
+    OkOnly = 3
 }
-# e.g. If (Ask("Save before exit?", "Exit", $QuestionType.YesNoCancel)) {...}
+
+# Ask a question. Show a standard question dialog with choosable buttons (similiar to MsgBox)
 function Ask($Question, $Header, $PossibleAnswers) {
     $Asking = New-Object System.Windows.Forms.Form -property @{
         Text = $Header
         Size = New-Object System.Drawing.Size(420, 180)
         StartPosition = "CenterScreen"
     }
+    
     $lbl = New-Object System.windows.Forms.Label -property @{
         Text = $Question
     }
     
-    $btn = New-Object System.windows.Forms.Button
-    $btn.Text = "OK/Cancel/Yes/No"
+    $btnYes = New-Object System.windows.Forms.Button -property @{
+        Text = "&Ja"
+        Location = New-Object System.Drawing.Point(129, 110)
+        Size = New-Object System.Drawing.Size(75, 23)
+        TabIndex = 1
+    }
+    $btnNo = New-Object System.windows.Forms.Button -property @{
+        Text = "&Nein"
+        Location = New-Object System.Drawing.Point(210, 110)
+        Size = New-Object System.Drawing.Size(75, 23)
+        TabIndex = 2
+    }
+    $btnCancel = New-Object System.windows.Forms.Button -property @{
+        Text = "&Abbrechen"
+        Location = New-Object System.Drawing.Point(291, 110)
+        Size = New-Object System.Drawing.Size(75, 23)
+        TabIndex = 3
+    }
     
     Switch($PossibleAnswers) {
         $QuestionType.YesNoCancel{
-            # Here we have to create the cancel Button and place it on the form
-            # no break
+            $Asking.Controls.Add($btnCancel)
         }
         
         $QuestionType.YesNo{
-           # we create Yes- and No-buttons and place it on the form
+           $Asking.Controls.Add($btnYes)
+           $Asking.Controls.Add($btnNo)
+           $btnYes.Add_Click({
+                $Asking.DialogResult = [System.Windows.Forms.DialogResult]::Yes
+            })
             break
         }
         
         $QuestionType.OKCancel{
-            # We create OK- and Cancel-Button and place it on the form
+            $Asking.Controls.Add($btnYes)
+            $Asking.Controls.Add($btnCancel)
+            $btnYes.Text = "OK"
+            $btnYes.Position = $btnNo.Position
+            $btnYes.Add_Click({
+                $Asking.DialogResult = [System.Windows.Forms.DialogResult]::OK
+            })
             break
         }
     }
     
-    $btn.Location = New-Object System.Drawing.Point(10, 30)
-    $btn.Add_Click({
-      # $Asking.DialogResult = System.Windows.Forms.DialogResult.OK
+    
+    $btnNo.Add_Click({
+      $Asking.DialogResult = [System.Windows.Forms.DialogResult]::No
     })
-    
-    
+    $btnCancel.Add_Click({
+      $Asking.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    })
     
     $Asking.Controls.Add($btn)
     $Asking.Controls.Add($lbl)
-    $Asking.ShowDialog()
-    If ($Asking.DialogResult = System.Windows.Forms.DialogResult.OK) {
-      return True
-    } else {
-      return False
-    }
+    
+    return $Asking.ShowDialog()
 }
